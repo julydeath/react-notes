ebc3f9ebd54c15a63baba3e2d1a7670fcb85a6da
julydeath <manojkarajada.mk@gmail.com>
2023-06-09 00:08:12 +0530

    chore : char limit added

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="max-w-[1240px] mx-auto mt-2 px-2">
-      <h1 className="text-5xl italic font-bold text-center mb-6 underline">
+      <h1 className="text-3xl md:text-5xl italic font-bold text-center mb-6 underline bg-gradient-to-r pb-4 rounded-md from-yellow-200 to-green-200">
         React Notes
       </h1>
       <NotesList list={list} GetNote={GetNote} DeleteNote={DeleteNote} />
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,24 +2,28 @@ import React, { useState } from 'react'
 
 const Input = ({ GetNote }) => {
     const [text, setText] = useState("")
-    const [count, setCount] = useState(200)
+    const charLimit = 300
 
     const handleChange = (e) => {
-        setText(e.target.value)
+        if (charLimit - e.target.value.length >= 0) {
+            setText(e.target.value)
+        }
     }
 
     const handleSubmit = () => {
-        GetNote(text)
-        setText("")
+        if (text.trim().length > 0) {
+            GetNote(text)
+            setText("")
+        }
     }
 
     return (
         <div className='col-span-4 md:col-span-2 lg:col-span-1'>
             <div className='bg-green-200 rounded-lg p-2 min-h-[280px] flex flex-col justify-between'>
-                <textarea className='bg-green-200 resize-none' cols="33" rows="10" value={text} onChange={handleChange}></textarea>
+                <textarea className='bg-green-200 resize-none' placeholder='Enter text here...' cols="33" rows="10" value={text} onChange={handleChange}></textarea>
                 <div className='flex justify-between'>
-                    <small>{count}</small>
-                    <button onClick={handleSubmit}>save</button>
+                    <small>{charLimit - text.length}</small>
+                    <button className='hover:underline' onClick={handleSubmit}>save</button>
                 </div>
             </div>
         </div>
